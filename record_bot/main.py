81fc709941e4ec615fb7689f560a3d6a3b06aa2f
Шаров Глеб <glscharow@yandex.ru>
2023-10-23 23:42:06 +0000

    First Customization

---
+++ record_bot/main.py
@@ -0,0 +1,46 @@
+import asyncio
+from loguru import logger
+from os import getenv
+from dotenv import load_dotenv
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+
+
+load_dotenv('../.env')
+TOKEN = getenv("BOT_TOKEN")
+
+
+dis = Dispatcher()
+
+
+@dis.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Дарова заебал, {message.from_user.full_name}!\n"
+                         f"Этот бот поможет не потеряться в очереди)")
+
+
+@dis.message(Command("show"))
+async def echo_handler(message: types.Message) -> None:
+    await message.answer("Тут будут показываться те, кто записан")
+
+
+@dis.message(Command("help"))
+async def echo_handler(message: types.Message) -> None:
+    await message.answer("Ой биляяя чо тебе не понятно здесь?")
+
+
+async def main() -> None:
+    bot = Bot(TOKEN)
+    await dis.start_polling(bot)
+
+
+if __name__ == "__main__":
+    try:
+        logger.info("The bot is up and running")
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
+    finally:
+        logger.info("The bot has stopped working")

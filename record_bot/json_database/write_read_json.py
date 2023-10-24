8a3ab9e8c7938ba9b10548a45b3b8e51b440f7de
Шаров Глеб <glscharow@yandex.ru>
2023-10-24 17:41:11 +0000

    Finished function read_json

--- record_bot/json_database/write_read_json.py
+++ record_bot/json_database/write_read_json.py
@@ -8,6 +8,16 @@ def write_json(dict_members: dict, name_file: str) -> None:
     :param name_file: File name
     :return: None
     """
-    with open(f'json_files/{name_file}.json', mode='w') as file:
-        json.dump(dict_members, file, indent=4)
-        file.close()
+    with open(f'json_files/{name_file}.json', mode='w', encoding='utf8') as file_write:
+        json.dump(dict_members, file_write, indent=4)
+        file_write.close()
+
+
+def read_json(name_file: str) -> None:
+    """
+    Function read json file with dict members
+    :param name_file: File name
+    :return: None
+    """
+    with open(f'json_files/{name_file}.json', mode='r', encoding='utf8') as file_read:
+        return json.load(file_read)

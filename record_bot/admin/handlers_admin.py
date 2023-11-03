bf94dd48119ab55b94ce08dd1f639d9c5370fd25
Шаров Глеб <glscharow@yandex.ru>
2023-11-03 22:01:13 +0000

    Contuine work in admin_panel

--- record_bot/admin/handlers_admin.py
+++ record_bot/admin/handlers_admin.py
@@ -1,3 +1,4 @@
+from loguru import logger
 
 from aiogram import F, types
 
@@ -7,17 +8,19 @@ from Bot_recording.record_bot.main import dis
 @dis.message()
 async def admin_panel(message: types.Message) -> None:
     if message.text == "Удалить черта":
-        await message.answer("Обработка удаления")
+        await admin_panel_delete(message=message)
 
     if message.text == "Поменять местами чертов":
-        await message.answer("Поменять")
+        await admin_panel_change(message=message)
 
 
 @dis.message()
 async def admin_panel_delete(message: types.Message) -> None:
-    pass
+    logger.info("Start function: admin_panel_delete")
+    await message.answer('Привет')
 
 
 @dis.message()
 async def admin_panel_change(message: types.Message) -> None:
+    logger.info("Start function: admin_panel_change")
     pass
